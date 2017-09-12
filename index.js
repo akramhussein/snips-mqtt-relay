07a8671842c38fe457e00801f3f94cbf2c51e71f
Sasha Lopoukhine <superlopuh@gmail.com>
2017-09-12 13:31:09 +0200

    merge bluetooth logic

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 #!/usr/bin/env node
 'use strict';
 
-const _mqtt = require('mqtt');
-const _ble = require('ble-relay');
 const options = require('node-options');
-const exec = require('child_process').exec;
+const startRelay = require('./lib/relay.js').startRelay;
+
 
 const opts = {
-    serviceUUID : '025A7775-49AA-42BD-BBDB-E2AE77782966',
-    characteristicUUID : 'F38A2C23-BC54-40FC-BED0-60EDDA139F47'
+    serviceUUID : '13EA4259-9D9E-42D1-A78B-638ED22CC768',
+    characteristicUUID : '81D97A06-7A2D-4A98-A2E2-41688E3D8283'
 }
 
 
@@ -21,8 +20,17 @@ if (result.errors) {
     process.exit(-1);
 }
 
-let mqttHost = 'localhost'
-let mqttPort = '9898'
+
+const ble = {
+    serviceUUID: opts.serviceUUID,
+    characteristicUUID: opts.characteristicUUID
+}
+
+
+const mqtt = {
+    host: 'localhost',
+    port: '9898'
+}
 
 if (result.args) {
     if (result.args.length !== 2) {
@@ -30,93 +38,9 @@ if (result.args) {
         process.exit(-2);
     }
 
-    mqttHost = result.args[0];
-    mqttPort = result.args[1];
+    mqtt.host = result.args[0];
+    mqtt.port = result.args[1];
 }
 
 
-var config = {
-    mqtt: {
-        host: mqttHost,
-        port: mqttPort
-    },
-    ble: {
-        displayName : 'Snips',
-        serviceUUID : opts.serviceUUID,
-        characteristicUUID : opts.characteristicUUID
-    }
-}
-
-const mqtt = _mqtt.connect(config.mqtt);
-
-const methods = {
-    postMQTT: (payload) => {
-        const mqtt = new Promise((resolve, reject) => {
-            mqtt.publish(payload.topic, new Buffer(payload.message, 'base64'), {}, (error, granted) => {
-                if (err) {
-                    resolve({ error });
-                } else {
-                    resolve({ success: "success" });
-                }
-            });
-        })
-    },
-    print: (payload) => {
-        return new Promis.resolve(console.log(payload))
-    },
-    shell: (payload) => {
-        return new Promise((resolve, reject) => {
-            console.log('execute shell');
-            exec(payload, function(error, stdout, stderr) {
-                console.log('finished executing shell');
-                const result = { error, stdout, stderr }
-                console.log('result: ' + result);
-                resolve(result)
-            });
-        });
-    }
-}
-
-let sendJSON = (json) => undefined;
-
-const sendResponse = (id, response) => {
-    sendJSON({response: {id: id, response: response}});
-}
-
-const sendMQTT = (topic, message) => {
-    sendJSON({mqtt: {topic: topic, message: message.toString('base64')}});
-}
-
-
-config.ble.onMessage = function(buffer) {
-    var message = JSON.parse(buffer);
-    console.log("received message" + JSON.stringify(message));
-    methods[message.function](message.payload)
-        .then((response) => {
-            console.log('rpc response: ' + response);
-            sendResponse(message.id, response);
-        });
-};
-
-config.ble.getReadData = function() { 
-    return new Buffer('Read data'); 
-};
-
-
-const ble = _ble.start(config.ble);
-
-sendJSON = (response) => {
-    const jsonText = JSON.stringify(response);
-    console.log('sending ' + jsonText);
-    const buff = new Buffer(jsonText);
-    ble.sendMessage(buff);
-}
-
-
-mqtt.on('message', function (topic, message) {
-    ble.sendMQTT(topic, message);
-});
-
-mqtt.on('connect', function () {
-    mqtt.subscribe('#');
-});
+startRelay({ mqtt, ble });
